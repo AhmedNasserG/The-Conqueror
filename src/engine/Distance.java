8fc020579b32fb4aa97dd5502f29323b5172fc1a
Mohammad Omar <omar@pop-os.localdomain>
2021-04-23 23:22:38 +0200

    Add: engine/Distance.java

---
+++ src/engine/Distance.java
@@ -0,0 +1,25 @@
+package engine;
+
+public class Distance {
+    private String from, to;
+    private int distance;
+
+    public Distance(String from, String to, int distance){
+        this.from = from;
+        this.to = to;
+        this.distance = distance;
+    }
+
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+}
