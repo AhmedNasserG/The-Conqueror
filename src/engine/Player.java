105261af4cd563e7699bf35567392b91474186d4
Mohammad Omar <omar@pop-os.localdomain>
2021-04-23 23:23:41 +0200

    Add: engine/Player.java

---
+++ src/engine/Player.java
@@ -0,0 +1,47 @@
+package engine;
+
+import units.Army;
+
+import java.util.ArrayList;
+
+public class Player {
+    private String name;
+    private ArrayList<City> controlledCities;
+    private ArrayList<Army> controlledArmies;
+    private double treasury;
+    private double food;
+
+    public Player(String name){
+        this.name = name;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<City> getControlledCities() {
+        return controlledCities;
+    }
+
+    public ArrayList<Army> getControlledArmies() {
+        return controlledArmies;
+    }
+
+    public double getTreasury() {
+        return treasury;
+    }
+
+    public double getFood() {
+        return food;
+    }
+
+
+    public void setTreasury(double treasury) {
+        this.treasury = treasury;
+    }
+
+    public void setFood(double food) {
+        this.food = food;
+    }
+}
