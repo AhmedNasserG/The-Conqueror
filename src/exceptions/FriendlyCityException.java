ada3fae4ff9b60aa5a2c29071f12e987cd5530b2
Ahmed Nasser <ahmednasser217217@gmail.com>
2021-04-25 13:19:02 +0200

    Add: FriendlyCityException class

---
+++ src/exceptions/FriendlyCityException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class FriendlyCityException extends ArmyException {
+
+    public FriendlyCityException() {}
+
+    public FriendlyCityException(String s) {
+        super(s);
+    }
+}
