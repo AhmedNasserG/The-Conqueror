448a2df9d34d310bce99768ba1a4c432bd86c6c7
Ahmed Nasser <ahmednasser217217@gmail.com>
2021-04-25 13:15:39 +0200

    Add: EmpireException class

---
+++ src/exceptions/EmpireException.java
@@ -0,0 +1,9 @@
+package exceptions;
+
+public abstract class EmpireException extends Exception {
+
+    public EmpireException(){}
+    public EmpireException(String s){
+        super(s);
+    }
+}
