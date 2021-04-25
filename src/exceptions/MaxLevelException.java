1378b5d45a1dcf8711e1ad1d88f09e2a8d72e178
Ahmed Nasser <ahmednasser217217@gmail.com>
2021-04-25 13:18:10 +0200

    Add: MaxLevelException class

---
+++ src/exceptions/MaxLevelException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class MaxLevelException extends BuildingException {
+
+    public MaxLevelException() {}
+
+    public MaxLevelException(String s) {
+        super(s);
+    }
+}
