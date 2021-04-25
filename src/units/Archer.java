dbb0ad0ada30e56a6cfe3182e785b90eba0c1494
SalahEl-Din Ebeed <salah.aped@gmail.com>
2021-04-25 14:58:47 +0200

    Add: Archer Class

---
+++ src/units/Archer.java
@@ -0,0 +1,9 @@
+package units;
+
+public class Archer extends Unit{
+
+    public Archer(int level, int maxSoldierCount, double idleUpkeep, double marchingUpkeep, double siegeUpkeep)
+    {
+        super(level,maxSoldierCount,idleUpkeep,marchingUpkeep,siegeUpkeep);
+    }
+}
