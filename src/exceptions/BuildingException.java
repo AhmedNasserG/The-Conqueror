81f5a97974283d291e91b23738bb2f5696bec87b
Ahmed Nasser <ahmednasser217217@gmail.com>
2021-04-25 13:16:44 +0200

    Add: BuildingException class

---
+++ src/exceptions/BuildingException.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+public abstract class BuildingException extends EmpireException {
+
+    public BuildingException() {}
+
+    public BuildingException(String s) {
+        super(s);
+    }
+
+}
