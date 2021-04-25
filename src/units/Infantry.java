a202c6cae17be4af5115cd03f76262e5392805a7
SalahEl-Din Ebeed <salah.aped@gmail.com>
2021-04-25 15:00:00 +0200

    Add: Infantry Class

---
+++ src/units/Infantry.java
@@ -0,0 +1,10 @@
+package units;
+
+public class Infantry extends Unit {
+
+    public Infantry (int level, int maxSoldierCount, double idleUpkeep, double marchingUpkeep, double siegeUpkeep)
+    {
+        super(level,maxSoldierCount,idleUpkeep,marchingUpkeep,siegeUpkeep);
+    }
+
+}
