6ed88a300156d1a3a1a435341c7b6979f04ebb3a
SalahEl-Din Ebeed <salah.aped@gmail.com>
2021-04-25 14:59:10 +0200

    Add: Cavalry Class

---
+++ src/units/Cavalry.java
@@ -0,0 +1,9 @@
+package units;
+
+public class Cavalry extends Unit{
+
+    public Cavalry(int level, int maxSoldierCount, double idleUpkeep, double marchingUpkeep, double siegeUpkeep)
+    {
+        super(level,maxSoldierCount,idleUpkeep,marchingUpkeep,siegeUpkeep);
+    }
+}
