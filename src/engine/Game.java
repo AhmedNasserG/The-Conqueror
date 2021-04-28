840fc6f3ab97ece655e0510dd71ab860c6d4492b
Ahmed Nasser <ahmednasser217217@gmail.com>
2021-04-29 00:23:14 +0200

    Update: finish Load Army method and the constructor in Game class

--- src/engine/Game.java
+++ src/engine/Game.java
@@ -6,8 +6,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.HashSet;
+
 
 public class Game {
     private Player player;
@@ -22,20 +22,18 @@ public class Game {
         distances = new ArrayList<>();
         currentTurnCount = 1;
         loadCitiesAndDistances();
-        player.getControlledCities().add(new City(playerCity));
-
-        //TODO: Carefully think about how will you initialize the army of the defending cities.
+        for (City city : availableCities) {
+            if (city.getName().equals(playerCity)) {
+                player.getControlledCities().add(city);
+                city.setDefendingArmy(null);
+            } else {
+                loadArmy(city.getName(), city.getName().toLowerCase() + "_army.csv");
+            }
+        }
     }
 
-    public void loadArmy(String cityName,String path) throws IOException {
-        //TODO: implement this method
-        if (cityName.equals(player.getControlledCities().get(0).getName())) {
-            player.getControlledCities().get(0).setDefendingArmy(null);
-            return;
-        }
-
-
-        Army army;
+    public void loadArmy(String cityName, String path) throws IOException {
+        Army army = null;
         for (City city : availableCities) {
             if (city.getName().equals(cityName)) {
                 army = new Army(cityName);
@@ -45,66 +43,61 @@ public class Game {
 
         ArrayList<Unit> units = new ArrayList<>();
         String currentLine;
-        FileReader fileReader= new FileReader(path);
+        FileReader fileReader = new FileReader(path);
         BufferedReader br = new BufferedReader(fileReader);
         while ((currentLine = br.readLine()) != null) {
             String[] line = currentLine.split(",");
-            switch (line[0]) {
-                case "Archer":
-                    switch (line[1]) {
-                        case "1":
-                            units.add(new Archer(1, 60, 0.4, 0.5, 0.6));
-                            break;
-                        case "2":
-                            units.add(new Archer(2, 60, 0.4, 0.5, 0.6));
-                            break;
-                        case "3":
-                            units.add(new Archer(3, 70, 0.5, 0.6, 0.7));
-                            break;
-                    }
-                    break;
-                case "Infantry":
-                    switch (line[1]) {
-                        case "1":
-                            units.add(new Infantry(1, 50, 0.5, 0.6, 0.7));
-                            break;
-                        case "2":
-                            units.add(new Infantry(2, 50, 0.5, 0.6, 0.7));
-                            break;
-                        case "3":
-                            units.add(new Infantry(3, 60, 0.6, 0.7, 0.8));
-                            break;
-                    }
-                    break;
-                case "Cavalry":
-                    switch (line[1]) {
-                        case "1":
-                            units.add(new Cavalry(1, 40, 0.6, 0.7, 0.75));
-                            break;
-                        case "2":
-                            units.add(new Cavalry(2, 40, 0.6, 0.7, 0.75));
-                            break;
-                        case "3":
-                            units.add(new Cavalry(3, 60, 0.7, 0.8, 0.9));
-                            break;
-                    }
-                    break;
-            }
+            units.add(loadUnit(line[0], line[1]));
         }
-
+        army.setUnits(units);
 
     }
-//    private Unit loadUnit(String type, int level) {
-//        if(type.equals("Archer")) {
-//
-//        }
-//        return null;
-//    }
+
+    private Unit loadUnit(String type, String level) {
+        switch (type) {
+            case "Archer":
+                switch (level) {
+                    case "1":
+                        return new Archer(1, 60, 0.4, 0.5, 0.6);
+
+                    case "2":
+                        return new Archer(2, 60, 0.4, 0.5, 0.6);
+
+                    case "3":
+                        return new Archer(3, 70, 0.5, 0.6, 0.7);
+
+                }
+            case "Infantry":
+                switch (level) {
+                    case "1":
+                        return new Infantry(1, 50, 0.5, 0.6, 0.7);
+
+                    case "2":
+                        return new Infantry(2, 50, 0.5, 0.6, 0.7);
+
+                    case "3":
+                        return new Infantry(3, 60, 0.6, 0.7, 0.8);
+
+                }
+            case "Cavalry":
+                switch (level) {
+                    case "1":
+                        return new Cavalry(1, 40, 0.6, 0.7, 0.75);
+                    case "2":
+                        return new Cavalry(2, 40, 0.6, 0.7, 0.75);
+                    case "3":
+                        return new Cavalry(3, 60, 0.7, 0.8, 0.9);
+
+                }
+        }
+        return null;
+    }
+
 
     private void loadCitiesAndDistances() throws IOException {
         HashSet<String> cities = new HashSet<>();
         String currentLine;
-        FileReader fileReader= new FileReader("distances.csv");
+        FileReader fileReader = new FileReader("distances.csv");
         BufferedReader br = new BufferedReader(fileReader);
         while ((currentLine = br.readLine()) != null) {
             String[] line = currentLine.split(",");
