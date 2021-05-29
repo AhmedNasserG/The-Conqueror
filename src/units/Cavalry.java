19ab11fe1f866d1f7b2b4760624e63daba2d1848
ahmednasserg <ahmednasser217217@gmail.com>
2021-05-30 00:08:48 +0200

    Add: Cavalry Values depends on level to Cavalry class

--- src/units/Cavalry.java
+++ src/units/Cavalry.java
@@ -2,7 +2,15 @@ package units;
 
 public class Cavalry extends Unit {
 
+    private static final double[][] CavalryValues = {{-1, -1, -1, -1}, {40, 0.6, 0.7, 0.75},
+            {40, 0.6, 0.7, 0.75}, {60, 0.7, 0.8, 0.9}};
+
     public Cavalry(int level, int maxSoldierCount, double idleUpkeep, double marchingUpkeep, double siegeUpkeep) {
         super(level, maxSoldierCount, idleUpkeep, marchingUpkeep, siegeUpkeep);
     }
+
+    public Cavalry(int level) {
+        super(level, (int) CavalryValues[level][0], CavalryValues[level][1], CavalryValues[level][2], CavalryValues[level][3]);
+    }
+
 }
