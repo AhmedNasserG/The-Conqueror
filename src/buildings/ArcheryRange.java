a6c22791c93a661f5f8e7b4b63531c60cb163af5
ahmednasserg <ahmednasser217217@gmail.com>
2021-05-31 15:44:18 +0200

    Add: Override Upgrade method in ArcheryRange class

--- src/buildings/ArcheryRange.java
+++ src/buildings/ArcheryRange.java
@@ -1,8 +1,25 @@
 package buildings;
 
+import exceptions.BuildingInCoolDownException;
+import exceptions.MaxLevelException;
+import exceptions.MaxRecruitedException;
+import units.Archer;
+import units.Unit;
+
 public class ArcheryRange extends MilitaryBuilding {
+
+    private final int[][] ArcheryRangeValuesUpgrades = {{700, 450}, {-1, 500}};
 
     public ArcheryRange() {
         super(1500, 800, 400);
     }
+
+    @Override
+    public void upgrade() throws BuildingInCoolDownException, MaxLevelException {
+        super.upgrade();
+        setUpgradeCost(ArcheryRangeValuesUpgrades[getLevel() - 2][0]);
+        setRecruitmentCost(ArcheryRangeValuesUpgrades[getLevel() - 2][1]);
+    }
+
+
 }
