bff67d052fe1dd29f83e66f21df4b5dba496c42c
ahmednasserg <ahmednasser217217@gmail.com>
2021-05-31 15:44:52 +0200

    Add: Override Upgrade method in Barracks class

--- src/buildings/Barracks.java
+++ src/buildings/Barracks.java
@@ -1,8 +1,27 @@
 package buildings;
 
+import exceptions.BuildingInCoolDownException;
+import exceptions.MaxLevelException;
+import exceptions.MaxRecruitedException;
+import units.Archer;
+import units.Cavalry;
+import units.Infantry;
+import units.Unit;
+
 public class Barracks extends MilitaryBuilding {
-	
+
+	private final int[][] BarracksValuesUpgrades = {{1500, 550}, {-1, 600}};
+
 	public Barracks() {
 		super(2000, 1000, 500);
 	}
+
+	@Override
+	public void upgrade() throws BuildingInCoolDownException, MaxLevelException {
+		super.upgrade();
+		setUpgradeCost(BarracksValuesUpgrades[getLevel() - 2][0]);
+		setRecruitmentCost(BarracksValuesUpgrades[getLevel() - 2][1]);
+	}
+
+
 }
