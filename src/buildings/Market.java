9d8f4e9de1663152a5e9793e780bb6592e0857b8
ahmednasserg <ahmednasser217217@gmail.com>
2021-05-31 15:43:03 +0200

    Add: Override Upgrade method in Market class

--- src/buildings/Market.java
+++ src/buildings/Market.java
@@ -1,8 +1,20 @@
 package buildings;
 
+import exceptions.BuildingInCoolDownException;
+import exceptions.MaxLevelException;
+
 public class Market extends EconomicBuilding{
+
 
 	public Market() {
 		super(1500, 700);
 	}
+
+	@Override
+	public void upgrade() throws BuildingInCoolDownException, MaxLevelException {
+		super.upgrade();
+		setUpgradeCost(1000);
+	}
+
+
 }
