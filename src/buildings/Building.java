d7f7bd8e0d8934b38434dde8d68ce189fbcc0496
ahmednasserg <ahmednasser217217@gmail.com>
2021-05-31 15:39:32 +0200

    Add: Upgrade method to Building class

--- src/buildings/Building.java
+++ src/buildings/Building.java
@@ -1,47 +1,61 @@
 package buildings;
 
+import exceptions.BuildingInCoolDownException;
+import exceptions.MaxLevelException;
+
 public abstract class Building {
 
-	private int cost;
-	private int level;
-	private int upgradeCost;
-	private boolean coolDown;
+    private int cost;
+    private int level;
+    private int upgradeCost;
+    private boolean coolDown;
 
 
-	public Building(int cost,int upgradeCost) {
-		this.cost = cost;
-		this.level = 1;
-		this.upgradeCost = upgradeCost;
-		this.coolDown = true;
-	}
+    public Building(int cost, int upgradeCost) {
+        this.cost = cost;
+        this.level = 1;
+        this.upgradeCost = upgradeCost;
+        this.coolDown = true;
+    }
 
-	// Getters
-	public int getCost() {
-		return cost;
-	}
+    public void upgrade() throws BuildingInCoolDownException, MaxLevelException {
+        if (coolDown) {
+            throw new BuildingInCoolDownException();
+        }
+        if (level == 3) {
+            throw new MaxLevelException();
+        }
+        level++;
+        coolDown = true;
+    }
 
-	public int getLevel() {
-		return level;
-	}
+    // Getters
+    public int getCost() {
+        return cost;
+    }
 
-	public int getUpgradeCost() {
-		return upgradeCost;
-	}
+    public int getLevel() {
+        return level;
+    }
 
-	public boolean isCoolDown() {
-		return coolDown;
-	}
+    public int getUpgradeCost() {
+        return upgradeCost;
+    }
 
-	// Setters
-	public void setLevel(int level) {
-		this.level = level;
-	}
+    public boolean isCoolDown() {
+        return coolDown;
+    }
 
-	public void setUpgradeCost(int upgradeCost) {
-		this.upgradeCost = upgradeCost;
-	}
+    // Setters
+    public void setLevel(int level) {
+        this.level = level;
+    }
 
-	public void setCoolDown(boolean coolDown) {
-		this.coolDown = coolDown;
-	}
+    public void setUpgradeCost(int upgradeCost) {
+        this.upgradeCost = upgradeCost;
+    }
+
+    public void setCoolDown(boolean coolDown) {
+        this.coolDown = coolDown;
+    }
 }
