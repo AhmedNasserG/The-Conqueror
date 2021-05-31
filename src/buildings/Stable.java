4fa752360f1bea3ba364c1dfbc7f99c37edb0d44
ahmednasserg <ahmednasser217217@gmail.com>
2021-05-31 15:45:14 +0200

    Add: Override Upgrade method in Stable class

--- src/buildings/Stable.java
+++ src/buildings/Stable.java
@@ -1,8 +1,25 @@
 package buildings;
 
+import exceptions.BuildingInCoolDownException;
+import exceptions.MaxLevelException;
+import exceptions.MaxRecruitedException;
+import units.Cavalry;
+import units.Unit;
+
 public class Stable extends MilitaryBuilding {
+
+	private final int[][] StableValuesUpgrades = {{2000, 650}, {-1, 700}};
 
 	public Stable() {
 		super(2500, 1500, 600);
 	}
+
+	@Override
+	public void upgrade() throws BuildingInCoolDownException, MaxLevelException {
+		super.upgrade();
+		setUpgradeCost(StableValuesUpgrades[getLevel() - 2][0]);
+		setRecruitmentCost(StableValuesUpgrades[getLevel() - 2][1]);
+	}
+
+
 }
