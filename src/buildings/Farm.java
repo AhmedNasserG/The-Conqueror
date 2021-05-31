81860b2968e0ea76a875f21f83f4e3fe2ed9fb4a
ahmednasserg <ahmednasser217217@gmail.com>
2021-05-31 15:41:51 +0200

    Add: Override Upgrade method in Farm class

--- src/buildings/Farm.java
+++ src/buildings/Farm.java
@@ -1,8 +1,21 @@
 package buildings;
 
+import exceptions.BuildingInCoolDownException;
+import exceptions.MaxLevelException;
+
 public class Farm extends EconomicBuilding{
+
+
 
 	public Farm() {
 		super(1000, 500);
 	}
+
+	@Override
+	public void upgrade() throws BuildingInCoolDownException, MaxLevelException {
+		super.upgrade();
+		setUpgradeCost(700);
+	}
+
+
 }
