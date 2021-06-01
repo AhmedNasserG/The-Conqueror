8a251a14486f6fab7b65b4a655c971b179227d53
ahmednasserg <ahmednasser217217@gmail.com>
2021-06-01 20:40:19 +0200

    Implement: Override attack method in Cavalry class

--- src/units/Cavalry.java
+++ src/units/Cavalry.java
@@ -1,13 +1,20 @@
 package units;
 
+import exceptions.FriendlyFireException;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Cavalry extends Unit {
 
     private static final double[][] CavalryValues = {{-1, -1, -1, -1}, {40, 0.6, 0.7, 0.75},
             {40, 0.6, 0.7, 0.75}, {60, 0.7, 0.8, 0.9}};
 
-    private final double[] targetArcherFactor = {0.5,0.6,0.7};
-    private final double[] targetInfantryFactor = {0.3,0.4,0.5};
-    private final double[] targetCavalryFactor = {0.2,0.2,0.3};
+    private final Map<Class, double[]> attackFactors = new HashMap<Class, double[]>() {{
+        put(Archer.class, new double[]{-1, 0.5, 0.6, 0.7});
+        put(Infantry.class, new double[]{-1, 0.3, 0.4, 0.5});
+        put(Cavalry.class, new double[]{-1, 0.2, 0.2, 0.3});
+    }};
 
     public Cavalry(int level, int maxSoldierCount, double idleUpkeep, double marchingUpkeep, double siegeUpkeep) {
         super(level, maxSoldierCount, idleUpkeep, marchingUpkeep, siegeUpkeep);
@@ -17,15 +24,13 @@ public class Cavalry extends Unit {
         super(level, (int) CavalryValues[level][0], CavalryValues[level][1], CavalryValues[level][2], CavalryValues[level][3]);
     }
 
-    public double[] getTargetArcherFactor() {
-        return targetArcherFactor;
-    }
-
-    public double[] getTargetInfantryFactor() {
-        return targetInfantryFactor;
-    }
-
-    public double[] getTargetCavalryFactor() {
-        return targetCavalryFactor;
+    @Override
+    public void attack(Unit target) throws FriendlyFireException {
+        super.attack(target);
+        int attackersCount = getCurrentSoldierCount();
+        int targetCount = target.getCurrentSoldierCount();
+        double attackFactor = attackFactors.get(target.getClass())[getLevel()];
+        target.setCurrentSoldierCount(targetCount - (int) attackFactor * attackersCount);
+        target.getParentArmy().handleAttackedUnit(target);
     }
 }
