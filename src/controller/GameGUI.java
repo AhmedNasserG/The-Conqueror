af692d07971be332aaa43068de2d8529b38ed653
MohammadOTaha <evilix9951@gmail.com>
2021-06-21 13:15:55 +0200

    Refactored: Stuff related to BattleView

--- src/controller/GameGUI.java
+++ src/controller/GameGUI.java
@@ -9,14 +9,18 @@ import units.*;
 import views.*;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class GameGUI
-        implements ActionListener, NewGameListener, StartMenuListener, CityViewListener, WorldMapListener, BattleListener, UnitListener {
+        implements ActionListener, NewGameListener, StartMenuListener, CityViewListener,
+        WorldMapListener, BattleListener, UnitListener, CardListener{
 
     private Game game;
+
     private final GameViews view;
 
     private Army a;
@@ -26,10 +30,6 @@ public class GameGUI
         view = new GameViews();
         view.setStartMenuView(new StartMenuView());
         view.getStartMenuView().setListener(this);
-//        view = new GameViews();
-//        game = new Game("OMAR", "Cairo");
-//        game.setListener(this);
-
     }
 
 
@@ -41,10 +41,10 @@ public class GameGUI
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == view.getBattleView().getNextMoveBtn()) {
+        if(e.getSource() == view.getBattleView().getNextMoveBtn()){
             try {
                 System.out.println("actionPerformed");
-                game.autoResolve(a, b);
+                game.autoResolve(a,b);
             } catch (FriendlyFireException friendlyFireException) {
                 friendlyFireException.printStackTrace();
             }
@@ -57,7 +57,10 @@ public class GameGUI
     }
 
     @Override
-    public void onBattleUpdated(Unit attacker, Unit target, String result) {
+    public void onBattleUpdated(Army unitParentArmy, String result1, String result2) {
+        JTextArea log = view.getBattleView().getBattleLog();
+        String RESULT = result1 + (game.getPlayer().getControlledArmies().contains(unitParentArmy) ? "Target" : "Player") + result2;
+        log.setText((log.getText() + "\n\n" + RESULT));
 //        JLabel resultsDisplay = new JLabel();
 //
 //        resultsDisplay.setText(result);
@@ -65,7 +68,8 @@ public class GameGUI
 //        view.getBattleView().setBattleResultsDisplay(resultsDisplay);
 //        view.getBattleView().revalidate();
 //        view.getBattleView().repaint();
-        System.out.println("yay");
+        System.out.println(RESULT);
+//        System.out.println("yay");
     }
 
     @Override
@@ -106,7 +110,6 @@ public class GameGUI
         view.setCityView(cityView);
         view.getCityView().revalidate();
         view.getCityView().repaint();
-
     }
 
     @Override
@@ -124,10 +127,6 @@ public class GameGUI
     }
 
 
-    public void autoResolveBattle() {
-        JPanel battlePanel = view.getBattleView().getBattlePanel();
-
-    }
 
     @Override
     public void onUpgradeClicked(BuildingPopUp buildingPopUp) {
@@ -158,6 +157,8 @@ public class GameGUI
         }
     }
 
+
+
     @Override
     public void onBuildingCardClicked(Building building, String whereToBuild) throws NotEnoughGoldException {
         if (whereToBuild != null) {
@@ -177,7 +178,14 @@ public class GameGUI
 
     @Override
     public void onUnitCardClicked(Unit unit) {
-
+        JPanel p = view.getBattleView().getUnitInfoPanel();
+        Card c = new Card(unit);
+        c.setAlignmentX(Component.CENTER_ALIGNMENT);
+        p.add(c);
+        view.getBattleView().revalidate();
+        view.getBattleView().repaint();
+//        p.setBackground(Color.orange);
+        System.out.println("ASdasdasd");
     }
 
     @Override
