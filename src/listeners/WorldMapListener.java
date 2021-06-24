3f057615d34dceea6d9ffa77c8d462776745c5ac
MohammadOTaha <evilix9951@gmail.com>
2021-06-24 17:46:03 +0200

    Edit: WorldMapListener

--- src/listeners/WorldMapListener.java
+++ src/listeners/WorldMapListener.java
@@ -1,12 +1,13 @@
 package listeners;
 
 import engine.City;
+import units.Army;
 
 import java.awt.event.ActionListener;
 
 public interface WorldMapListener extends ActionListener, CardListener {
-    void onManualAttackChosen() throws InterruptedException;
-    void onAutoResolveChosen() throws InterruptedException;
+    void onManualAttackChosen(String battleMode, Army playerArmy, City targetCity) throws InterruptedException;
+    void onAutoResolveChosen(String battleMode, Army playerArmy, City targetCity) throws InterruptedException;
     void onCityClicked(City city) ;
     void onCityCardClicked(City city);
     void onAttackClicked();
