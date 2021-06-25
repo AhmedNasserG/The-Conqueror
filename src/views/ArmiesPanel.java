22ad428d16d1b84f09d7ca8447328dc04c64b74f
SalahEl-Din Ebeed <salah.aped@gmail.com>
2021-06-25 17:29:33 +0200

    Edit: fix the scroll bar in the cityView and WorldMap View

--- src/views/ArmiesPanel.java
+++ src/views/ArmiesPanel.java
@@ -11,30 +11,37 @@ public class ArmiesPanel extends JPanel {
 
     public ArmiesPanel(ArrayList<Army> defendingArmies,ArrayList<Army> controlledArmies, CardListener listener) {
         super();
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+
+        //TODO: For the scroll bar to show we need to have a method to update the panel because the size of the armies changed
         JPanel defendingArmyPanel = new JPanel();
-        defendingArmyPanel.setLayout(new BoxLayout(defendingArmyPanel,BoxLayout.Y_AXIS));
+        defendingArmyPanel.setPreferredSize(new Dimension(this.getWidth(),defendingArmies.size()*100));
+        defendingArmyPanel.setLayout(new GridLayout(0,1));
         JPanel controlledArmiesPanel = new JPanel();
-        controlledArmiesPanel.setLayout(new BoxLayout(controlledArmiesPanel,BoxLayout.Y_AXIS));
+        controlledArmiesPanel.setPreferredSize(new Dimension(this.getWidth(),controlledArmies.size()*150));
+        controlledArmiesPanel.setLayout(new GridLayout(0,1));
+
+
         for (Army army : defendingArmies) {
             Card armyCard = new Card(army);
             armyCard.setListener(listener);
             armyCard.setAlignmentX(Component.CENTER_ALIGNMENT);
             defendingArmyPanel.add(armyCard);
-            defendingArmyPanel.add(Box.createVerticalStrut(10));
         }
         for (Army army : controlledArmies) {
             Card armyCard = new Card(army);
             armyCard.setListener(listener);
             armyCard.setAlignmentX(Component.CENTER_ALIGNMENT);
             controlledArmiesPanel.add(armyCard);
-            controlledArmiesPanel.add(Box.createVerticalStrut(10));
         }
         defendingArmyPanel.setBorder(BorderFactory.createTitledBorder("Defending Armies"));
         controlledArmiesPanel.setBorder(BorderFactory.createTitledBorder("Controlled Armies"));
+
+        this.setAlignmentX(Component.CENTER_ALIGNMENT);
+
         this.add(new JScrollPane(defendingArmyPanel));
         this.add(new JScrollPane(controlledArmiesPanel));
-        this.setAlignmentX(Component.CENTER_ALIGNMENT);
-        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
     }
 
